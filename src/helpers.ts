7b53c86a81a65095fcb4bc317d6de1cef0eee6a9
Shoaib Shakeel <shoaib.shakeel@rebtel.com>
2020-08-23 02:11:43 +0200

    minor updates

--- src/helpers.ts
+++ src/helpers.ts
@@ -4,28 +4,28 @@ import { window } from 'vscode';
  * Prompts user for inputting quotes using window.showInputBox().
  */
 export async function chooseQuoteSymbol() {
-	const result = await window.showInputBox({
-		value: '\"',
-		placeHolder: 'Enter single (\') or double (\") quotes',
-		validateInput: text => text !== '\'' && text !== '\"' ? 'Only single or double quote character is allowed' : null
-	});
-	return result;
+    const result = await window.showInputBox({
+        value: '\"',
+        placeHolder: 'Enter single (\') or double (\") quotes',
+        validateInput: text => text !== '\'' && text !== '\"' ? 'Only single or double quote character is allowed' : null
+    });
+    return result;
 }
 
 export async function chooseDelimitSymbol() {
-	const result = await window.showInputBox({
-		value: ',',
-		placeHolder: 'Enter delimit symbol. (\\n for new-line)',
-		validateInput: text => !text ? 'Atleast one character is required' : null
-	});
-	return result;
+    const result = await window.showInputBox({
+        value: ',',
+        placeHolder: 'Enter delimit symbol. (\\n for new-line)',
+        validateInput: text => !text ? 'Atleast one character is required' : null
+    });
+    return result;
 }
 
 export async function chooseSymbolToUnquote(): Promise<boolean> {
-	const result = await window.showQuickPick(["Single Quote (\')", "Double Quote (\")"],
-		{
-			placeHolder: "Select symbol to unquote"
-		}
-	);
-	return result == 'Single Quote (\')';
+    const result = await window.showQuickPick(["Single Quote (\')", "Double Quote (\")"],
+        {
+            placeHolder: "Select symbol to unquote"
+        }
+    );
+    return result == 'Single Quote (\')';
 }
